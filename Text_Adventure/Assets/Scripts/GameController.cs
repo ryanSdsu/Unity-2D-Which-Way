a98efb568933235d5fd37ad68c93d2ecd318ce0b
Ryan Ragasa <RJ@ryans-imac.home>
2017-09-15 10:37:40 -0700

    Text-Adventure Game added i.e. Initial Setup - V0.1
    Created the initial set up for the game

---
+++ Text_Adventure/Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text displayText;
+
+	[HideInInspector] public RoomNavigation RoomNavigation;
+	// Use this for initialization
+
+	List<string> actionLog = new List<string> ();
+	void Awake () {
+		RoomNavigation = GetComponent<RoomNavigation> ();
+	}
+
+	void Start(){
+		DisplayRoomText ();
+		DisplayLoggedText ();
+	}
+
+	public void DisplayLoggedText() {
+		string logAsText = string.Join ("\n", actionLog.ToArray ());
+
+		displayText.text = logAsText;
+	}
+
+	public void DisplayRoomText()
+	{
+		string combinedText = RoomNavigation.currentRoom.description + "\n";
+		LogStringWithReturn (combinedText);
+	}
+
+	public void LogStringWithReturn(string stringToAdd) {
+	
+		actionLog.Add (stringToAdd + "\n");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
